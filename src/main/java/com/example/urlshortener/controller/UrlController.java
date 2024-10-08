901c6795884af2d5bd09216c185fcef81e4ed211
Alexandre Destro <aalexandredestro@gmail.com>
2024-10-08 16:08:21 -0300

    commit

---
+++ src/main/java/com/example/urlshortener/controller/UrlController.java
@@ -0,0 +1,50 @@
+package com.example.urlshortener.controller;
+
+import com.example.urlshortener.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping()
+public class UrlController {
+
+    @Autowired
+    private UrlService urlShortenerService;
+
+    @GetMapping()
+    public String getM() {
+        return "Encurtador de Url! ....... método: /encurtar?url=https://www.exemplo.com";
+    }
+
+    //get query string
+    @GetMapping("/encurtar")
+    public ResponseEntity<String> shortenUrl(@RequestParam("url") String originalUrl) {
+        try {
+            String shortenedUrl = urlShortenerService.shortenUrl(originalUrl);
+            return ResponseEntity.ok(shortenedUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao encurtar a URL: " + e.getMessage());
+        }
+    }
+    //post json
+    @PostMapping("/encurtar")
+    public ResponseEntity<String> shortenUrlPost(@RequestBody String originalUrl) {
+        try {
+            String shortenedUrl = urlShortenerService.shortenUrl(originalUrl);
+            return ResponseEntity.ok(shortenedUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao encurtar a URL: " + e.getMessage());
+        }
+    }
+    @GetMapping("/sobre")
+    @ResponseBody
+    public HashMap<String, String> sobre() {
+        HashMap<String, String> sobre = new HashMap<>();
+        sobre.put("Estudante", "Alexandre Destro Zanoni");
+        sobre.put("Projeto", "Encurtador de Url top 2024");
+        return sobre;
+    }
+}
---
+++ src/main/java/com/example/urlshortener/service/UrlService.java
@@ -0,0 +1,28 @@
+package com.example.urlshortener.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UrlService {
+
+    private static final String SHORTENER_API_URL = "https://ulvis.net/api.php?url=";
+
+    public String shortenUrl(String originalUrl) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String apiUrl = SHORTENER_API_URL + originalUrl;
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, null, String.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return ("Sua Url encurtada: " + response.getBody() +"<br> Sua Url original: " + originalUrl);
+        } else {
+            throw new RuntimeException("Erro ao encurtar a URL: " + response.getStatusCode());
+        }
+    }
+}
